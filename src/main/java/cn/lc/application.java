9fac1269dc419227ab88a5f4a2ad390438ccb6ae
LC-CPU <2464797352@qq.com>
2022-07-14 14:28:50 +0800

    spring环境搭建

---
+++ src/main/java/cn/lc/application.java
@@ -0,0 +1,16 @@
+package cn.lc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liucheng
+ * @date 2022/7/14
+ * @description
+ */
+@SpringBootApplication
+public class application {
+    public static void main(String[] args) {
+        SpringApplication.run(application.class,args);
+    }
+}
